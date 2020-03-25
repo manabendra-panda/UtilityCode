8f7882c362b57b47ffc31bee31104b3055956c5c
Panda, Manabendra <manabendra.panda@accenture.com>
2020-03-25 13:54:17 +0530

    project commit 2

---
+++ CSharpSolutions/FileUpload/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace FileUpload
+{
+    public class Program
+    {
+        private static string GetKeyVaultEndpoint() => "https://kv-demo-dev.vault.azure.net/";
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                //.ConfigureAppConfiguration((ctx, builder) =>
+                //{
+                //    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                //    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                //    {
+                //        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                //        var keyVaultClient = new KeyVaultClient(
+                //           new KeyVaultClient.AuthenticationCallback(
+                //              azureServiceTokenProvider.KeyVaultTokenCallback));
+                //        builder.AddAzureKeyVault(
+                //           keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                //    }
+                //})
+                .UseStartup<Startup>();
+    }
+}
